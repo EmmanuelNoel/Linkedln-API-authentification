04abebc05ba616cf5c35b5eff05a79f68226fa0b
Emmanuel <EmmanuelNoel>
2024-05-11 03:17:57 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '782h8kl71svlyt', // your App ID
+      'clientSecret': 'WPL_AP0.lKKCDa4A51Ilb2Tw.Mzg4Mjc0MzU5OQ==', // your App Secret
+      'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+    }
+  }
